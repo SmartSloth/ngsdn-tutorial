7aac88e3ccec545793be076fa1009025b35daa99
SmartSloth <laurenciasloth@gmail.com>
2021-05-13 01:08:49 +0800

    traffic test successfully

--- topo/generate_topo.py
+++ topo/generate_topo.py
@@ -4,15 +4,15 @@ import networkx as nx
 
 from networkx.utils import pairwise
 
-subset_sizes = [5, 5, 4, 3, 2, 4, 4, 3]
+subset_sizes = [8, 4, 4, 2, 2, 4, 4, 8]
 subset_color = [
     "gold",
     "violet",
     "violet",
-    "violet",
-    "violet",
     "limegreen",
     "limegreen",
+    "violet",
+    "violet",
     "darkorange",
 ]
 
@@ -25,13 +25,38 @@ def multilayered_graph(*subset_sizes):
         G.add_nodes_from(layer, layer=i)
     for layer1, layer2 in pairwise(layers):
         G.add_edges_from(itertools.product(layer1, layer2))
+    layer1 = layers[0]
+    layer2 = layers[1]
+    # for tor in layer1:
+    #     for s1 in range(layer2[0]
+    # print("layer1 = %s, layer2 = %s" % (type(layer1), layer2))
     return G
 
 
 G = multilayered_graph(*subset_sizes)
+nodes = G.nodes()
+edges = G.edges()
+links_map = {}
+with open("generated_topo", "w+") as f:
+    for n in nodes:
+        f.write("s" + str(n))
+        f.write(",")
+        links_map[n] = 0
+    f.write("\n")
+    # print(links_map)
+    for e in edges:
+        src, dst = e[0], e[1]
+        src_cur = links_map.get(src)
+        dst_cur = links_map.get(dst)
+        # print(src_cur, dst_cur)
+        f.write("s%d:%d-s%d:%d" % (src, src_cur, dst, dst_cur))
+        f.write("\n")
+        links_map[src] = src_cur + 1
+        links_map[dst] = dst_cur + 1
+
 color = [subset_color[data["layer"]] for v, data in G.nodes(data=True)]
 pos = nx.multipartite_layout(G, subset_key="layer")
 plt.figure(figsize=(8, 8))
-nx.draw(G, pos, node_color=color, with_labels=False)
+nx.draw(G, pos, node_color=color, with_labels=True)
 plt.axis("equal")
 plt.show()
